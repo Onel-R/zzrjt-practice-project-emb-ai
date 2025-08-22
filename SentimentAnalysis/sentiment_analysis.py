0bfd466d275c66f31d2b38f89cf663d8b49ac36c
Onel-R <leonreuben@yahoo.com>
2025-08-22 18:35:42 -0400

    Error hadling and lynt fixes

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -6,8 +6,12 @@ def sentiment_analyzer(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     formatted = json.loads(response.text)
-
-    label = formatted['documentSentiment']['label']
-    score = formatted['documentSentiment']['score']
+    
+    if response.status_code == 200:
+        label = formatted['documentSentiment']['label']
+        score = formatted['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
 
     return {'label': label, 'score': score}
--- server.py
+++ server.py
@@ -7,7 +7,7 @@
 from flask import Flask, render_template, request
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-#Initiate the flask app : TODO
+#Initiate the flask app :
 app = Flask('Sentiment Analyzer')
 
 @app.route("/sentimentAnalyzer")
@@ -17,23 +17,24 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
-    text_To_Analyze = request.args.get('textToAnalyze')
-    response = sentiment_analyzer(text_To_Analyze)
+    text_T_Analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_T_Analyze)
     label = response['label']
     score = response['score']
 
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    if label == None:
+        return "Invalid input! Try again."
+    else:
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
     return render_template('index.html')
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''
     app.run(host="0.0.0.0", port=5000)
